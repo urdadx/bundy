de7ca07f91e480455d58f177f4a4e699f54823ae
urdadx <nerdyshinobi12@gmail.com>
2026-01-26 13:59:56 +0000

    fixed navigation

--- apps/web/src/components/lesson-banner.tsx
+++ apps/web/src/components/lesson-banner.tsx
@@ -1,51 +1,64 @@
 import { MoveLeftIcon, NotebookText } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
 
 type BattleBanner = {
-  name: string;
-  color: string;
-  description?: string;
-  stageId?: string;
-  order?: number;
+	name: string;
+	color: string;
+	description?: string;
+	stageId?: string;
+	order?: number;
 };
 
 export function BattleBanner({ name, color, description, stageId, order }: BattleBanner) {
-  const bgClass =
-    {
-      primary: "bg-[#58cc02]",
-      secondary: "bg-[#1cb0f6]",
-      danger: "bg-[#ff4b4b]",
-      super: "bg-[#ce82ff]",
-      highlight: "bg-[#ff9600]",
-      golden: "bg-[#ffc800]",
-      locked: "bg-[#e5e5e5]",
-    }[color || "primary"] || "bg-[#58cc02]";
-    
-    const navigate = useRouter()
+	const bgClass =
+		{
+			primary: "bg-[#58cc02]",
+			secondary: "bg-[#1cb0f6]",
+			danger: "bg-[#ff4b4b]",
+			super: "bg-[#ce82ff]",
+			highlight: "bg-[#ff9600]",
+			golden: "bg-[#ffc800]",
+			locked: "bg-[#e5e5e5]",
+		}[color || "primary"] || "bg-[#58cc02]";
 
-  return (
-    <header
-      className={cn("flex w-full items-center justify-between rounded-xl p-5 text-white", bgClass)}
-    >
-      <div className="space-y-1">
-        <div className="flex items-center gap-2">
-          <MoveLeftIcon onClick={() => navigate({ to: "/arena/lessons"})} className="size-5" />
-          <h3 className="text-sm uppercase font-bold text-white/90">
-            World {order || 1}: <span className="uppercase">{name}</span>
-          </h3>
-        </div>
-        {description && (
-          <p className="text-base tracking-wider font-semibold sm:text-lg ">{description}</p>
-        )}
-      </div>
-      <Button variant="immersive" className="max-xl:px-4" size="lg" asChild disabled={!stageId}>
-        <Link to="/arena/playground" search={{ stageId }}>
-          <NotebookText className="" />
-          <span className="ml-2 max-xl:hidden">Continue</span>
-        </Link>
-      </Button>
-    </header>
-  );
+	const navigate = useNavigate();
+
+	const handleGoBack = () => {
+		navigate({ to: "/arena/lessons" });
+	};
+
+	return (
+		<header
+			className={cn(
+				"flex w-full items-center justify-between rounded-xl p-5 text-white",
+				bgClass,
+			)}>
+			<div className="space-y-1">
+				<div className="flex items-center gap-2">
+					<MoveLeftIcon onClick={handleGoBack} className="size-5" />
+					<h3 className="text-sm uppercase font-bold text-white/90">
+						World {order || 1}: <span className="uppercase">{name}</span>
+					</h3>
+				</div>
+				{description && (
+					<p className="text-base tracking-wider font-semibold sm:text-lg ">
+						{description}
+					</p>
+				)}
+			</div>
+			<Button
+				variant="immersive"
+				className="max-xl:px-4"
+				size="lg"
+				asChild
+				disabled={!stageId}>
+				<Link to="/arena/playground" search={{ stageId }}>
+					<NotebookText className="" />
+					<span className="ml-2 max-xl:hidden">Continue</span>
+				</Link>
+			</Button>
+		</header>
+	);
 }
