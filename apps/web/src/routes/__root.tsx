0e3ffe863a56b5cc796f46f5ff6337348943e192
urdadx <nerdyshinobi12@gmail.com>
2026-03-04 10:14:11 +0000

    add pear support widget

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -8,48 +8,48 @@ import "../index.css";
 import { seo } from "@/lib/seo";
 
 export interface RouterAppContext {
-  trpc: typeof trpc;
-  queryClient: QueryClient;
+	trpc: typeof trpc;
+	queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<RouterAppContext>()({
-  component: RootComponent,
-  head: () => ({
-    meta: [
-      {
-        title: "wordsearch",
-      },
-      {
-        name: "description",
-        content: "Wordsearch adventure game for learning and fun!",
-      },
-      ...seo({
-        title: "Bundy - A wordsearch adventure game",
-        description:
-          "Play Bundy, an exciting wordsearch adventure game that challenges your vocabulary and spelling skills while having fun!",
-        keywords: "wordsearch, adventure game, vocabulary, spelling, learning, fun",
-        image: "/og.png",
-      }),
-    ],
-    links: [
-      {
-        rel: "icon",
-        href: "/favicon/favicon.ico",
-      },
-    ],
-  }),
+	component: RootComponent,
+	head: () => ({
+		meta: [
+			{
+				title: "wordsearch",
+			},
+			{
+				name: "description",
+				content: "Wordsearch adventure game for learning and fun!",
+			},
+			...seo({
+				title: "Bundy - A wordsearch adventure game",
+				description:
+					"Play Bundy, an exciting wordsearch adventure game that challenges your vocabulary and spelling skills while having fun!",
+				keywords: "wordsearch, adventure game, vocabulary, spelling, learning, fun",
+				image: "/og.png",
+			}),
+		],
+		links: [
+			{
+				rel: "icon",
+				href: "/favicon/favicon.ico",
+			},
+		],
+	}),
 });
 
 function RootComponent() {
-  const { theme } = useTheme();
+	const { theme } = useTheme();
 
-  return (
-    <>
-      <div className="grid grid-rows-[auto_1fr] h-svh">
-        <Outlet />
-      </div>
-      <Toaster theme={theme as "light" | "dark"} richColors />
-      <ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
-    </>
-  );
+	return (
+		<>
+			<div className="grid grid-rows-[auto_1fr] h-svh">
+				<Outlet />
+			</div>
+			<Toaster theme={theme as "light" | "dark"} richColors />
+			{/* <ReactQueryDevtools position="bottom" buttonPosition="bottom-right" /> */}
+		</>
+	);
 }
